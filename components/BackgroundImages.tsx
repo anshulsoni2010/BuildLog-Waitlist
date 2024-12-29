a94dada050d152cfa557453a3d08c7eb2ffeb92e
Anshul Soni <102960277+anshulsoni2010@users.noreply.github.com>
2024-12-29 20:55:58 +0530

    Update BackgroundImages.tsx

--- components/BackgroundImages.tsx
+++ components/BackgroundImages.tsx
@@ -10,7 +10,7 @@ const BackgroundImages = () => {
       {/* bottom light beams */}
       <div className="w-[800px] sm:w-[1400px] rotate-[25deg] absolute -bottom-44 border-t border-zinc-800 -left-44 h-56 bg-gradient-to-b from-[#181818]/70 to-black/50 " />
       <div className="w-[400px] sm:w-[800px] rotate-[23deg] border-t border-zinc-800 absolute -bottom-44 -left-36 h-56 bg-gradient-to-b from-[#181818]/70 to-black/50 " />
-{/* 
+
       <img
         src="/3ds/holo-sphere.png"
         alt="background"
@@ -20,10 +20,10 @@ const BackgroundImages = () => {
         src="/3ds/coins-ball.png"
         alt="background"
         className="absolute object-cover size-36 md:top-12 md:right-12 sm:size-48 sm:top-6 sm:right-6 -top-3 -right-3 "
-      /> */}
+      /> 
 
       {/* center */}
-      {/* <img
+      <img
         src="/3ds/holo-cylinder.png"
         alt="background"
         className="hidden lg:block absolute object-cover left-[20%] top-1/2 size-32 "
@@ -32,10 +32,10 @@ const BackgroundImages = () => {
         src="/3ds/holo-diamond.png"
         alt="background"
         className="hidden lg:block absolute object-cover right-[20%] top-1/2 size-32 "
-      /> */}
+      />
 
       {/* Bottom */}
-      {/* <img
+      <img
         src="/3ds/holo-abstract-shape.png"
         alt="background"
         className="absolute object-cover size-36 sm:size-48 sm:bottom-6 md:bottom-12 md:left-12 sm:left-6 -bottom-3 -left-3 "
@@ -44,7 +44,7 @@ const BackgroundImages = () => {
         src="/3ds/cube-helix.png"
         alt="background"
         className="absolute hidden object-cover sm:block md:bottom-12 md:right-12 size-56 bottom-6 right-6"
-      /> */}
+      />
     </div>
   );
 };
