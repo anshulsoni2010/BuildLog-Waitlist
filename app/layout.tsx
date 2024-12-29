97a32f1a455c767bbb98bb93cbde9e91362f53c8
Anshul Soni <anshulsoni2022@gmail.com>
2024-12-29 21:07:58 +0530

    added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,35 +3,40 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
 import { Toaster } from "react-hot-toast";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Join Waitlist",
-  description: "Join our waitlist to get early access",
+    title: "Join Waitlist",
+    description: "Join our waitlist to get early access",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Providers>{children}</Providers>
-        <Toaster
-          position="top-center"
-          toastOptions={{
-            style: {
-              background: '#18181B',
-              color: '#fff',
-              minWidth: '250px',
-            },
-            duration: 4000,
-          }}
-        />
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={inter.className}>
+                <Providers>{children}
+
+                    <Analytics />
+
+                </Providers>
+                <Toaster
+                    position="top-center"
+                    toastOptions={{
+                        style: {
+                            background: '#18181B',
+                            color: '#fff',
+                            minWidth: '250px',
+                        },
+                        duration: 4000,
+                    }}
+                />
+            </body>
+        </html>
+    );
 }
